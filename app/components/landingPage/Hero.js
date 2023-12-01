78c6516332e9d50b1bdca764e50f6ee7188f84cb
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-12-01 15:16:24 +0000

    Friday, December 1, 2023 at 4:16 PM West Africa Standard Time

---
+++ app/components/landingPage/Hero.js
@@ -0,0 +1,19 @@
+"use client";
+
+function Hero() {
+  return (
+    <div>
+      <div>
+        <span className="text-white text-5xl font-normal ">Get Your</span>
+       
+        <span className="text-white text-5xl font-semibold mx-3 ">Confidence</span>
+       
+        <span className="text-white text-5xl font-normal ">Again</span>
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -1,20 +1,21 @@
 import { FormProvider } from "../context/form/userProfile/FormContext";
 import Form from "./components/form/userProfile/template";
+import Hero from "./components/landingPage/Hero";
 import Navbar from "./components/landingPage/navbar";
 import PersonaliseScreen from "./components/ui/personaliseScreen/template";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center bg-grayDark justify-center">
-     
-      <PersonaliseScreen
+     <Hero/>
+      {/* <PersonaliseScreen
         title="Get Personalized Outfit Recommendations"
         description={`At Mestyle, we're dedicated to helping you look and feel your best. To provide you with the most relevant outfit suggestions, please share some information with us. Rest assured that your data is kept secure and used only to enhance your style journey.`}
       >
         <FormProvider>
           <Form />
         </FormProvider>
-      </PersonaliseScreen> 
+      </PersonaliseScreen>  */}
     </main>
   );
 }
