1307c5e1368f024cc47e757305339f38136a0a44
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-12-01 15:06:58 +0100

    added props to button

--- app/page.js
+++ app/page.js
@@ -6,16 +6,15 @@ import PersonaliseScreen from "./components/ui/personaliseScreen/template";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center bg-grayDark justify-center">
-     <Navbar/>
      
-      {/* <PersonaliseScreen
+      <PersonaliseScreen
         title="Get Personalized Outfit Recommendations"
         description={`At Mestyle, we're dedicated to helping you look and feel your best. To provide you with the most relevant outfit suggestions, please share some information with us. Rest assured that your data is kept secure and used only to enhance your style journey.`}
       >
         <FormProvider>
           <Form />
         </FormProvider>
-      </PersonaliseScreen> */}
+      </PersonaliseScreen> 
     </main>
   );
 }
