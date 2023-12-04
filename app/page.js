5886f6f0221f237fc28b99e7b25e4b9d642191f0
Edidiong <etokedidionge@gmail.com>
2023-12-04 17:47:21 +0100

    resolve conflict

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 'use client'
-import { FormProvider } from "../context/form/userProfile/FormContext";
-import Form from "./components/form/userProfile/template";
 import HomePage from "./components/landingpage/template";
-import PersonaliseScreen from "./components/ui/personaliseScreen/template";
+
 
 export default function Home() {
   return (
