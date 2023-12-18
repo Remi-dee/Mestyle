0584eb8160c99b4e23335e4f2d8dc5d1a6015031
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-12-18 15:51:42 +0000

    Monday, December 18, 2023 at 4:51 PM West Africa Standard Time

--- app/components/landingPage/Explore_comp2.js
+++ app/components/landingPage/Explore_comp2.js
@@ -3,13 +3,22 @@ import React from "react";
 import Button from "../ui/button/template";
 
 function Explore() {
+  const header = "Get Personalized Outfit Recommendations".split();
+
+  const paragraph =
+    "Browse our curated collection of outfits and trends for every occasion. There&apos;s no need to create an account start exploring right away!".split(
+      ""
+    );
+
   return (
     <>
       <section className=" md:h-screen  p-20 md:p-24  bg-[url('/images/clothing-rack-with-floral-hawaiian-shirts-hangers-hat-1.png')] bg-no-repeat bg-cover">
         <div className=" lg:p-4 -ml-8 lg:ml-2 md:max-lg:py-8  flex space-y-8 flex-col justify-center">
           <div className="lg:w-[80%] w-full lg:pt-7 md:w-4/5 space-y-4 text-white">
             <p className="text-2xl md:text-4xl font-bold">
-              Get Personalized Outfit Recommendations
+              {header.map((letter, index) => {
+                return { letter };
+              })}
             </p>
             <p className=" md:text-2xl">
               Browse our curated collection of outfits and trends for every
