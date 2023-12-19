19f143f2cb7a04d48ef4be8892f7540d6b830883
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-12-19 14:20:31 +0000

    Tuesday, December 19, 2023 at 3:20 PM West Africa Standard Time

--- app/components/landingPage/Contact_comp.js
+++ app/components/landingPage/Contact_comp.js
@@ -3,7 +3,7 @@ import Button from "../ui/button/template";
 function Contact() {
   return (
     <>
-      <section className="h-screen  pl-6 md:pl-10 lg:pl-[98px]   bg-[url('/images/bg-image-2.png')] bg-no-repeat bg-cover">
+      <section className="h-screen z-50 pl-6 md:pl-10 lg:pl-[98px]   bg-[url('/images/bg-image-2.png')] bg-no-repeat bg-cover">
         <div className="h-full w-full p-4 flex flex-col justify-center  space-y-8">
           <div className="lg:w-3/6 w-4/5 lg:space-y-6 space-y-4 text-white">
             <p className="text-2xl md:text-4xl font-bold ">
--- app/components/landingPage/CreatorCTA_comp.js
+++ app/components/landingPage/CreatorCTA_comp.js
@@ -9,44 +9,43 @@ import waterMark from "@/public/images/creator/waterMark.png";
 function CreatorCTA() {
   return (
     <>
-      <section className="relative lg:flex gap-2 bg-black w-full h-screen space-y-4 lg:px-24 lg:pt-24 lg:pb-0 px-6 pt-6 pb-6">
-        <div className="  ">
-          <Image
-            width={200}
-            height={null}
-            src={stackLeft}
-            alt=""
-            className="absolute top-20 left-25  h-[300px] w-[200px] rounded-tl-[20px] rounded-tr-[20px]  "
-          />
+      <section className="flex  gap-40  bg-black w-full h-screen space-y-4 ">
+        <div className=" relative ">
+          <div className=" ">
+            <Image
+              width={200}
+              height={null}
+              src={stackLeft}
+              alt=""
+              className="absolute top-20 left-[130px]  h-[300px] w-[200px] rounded-tl-[20px] rounded-tr-[20px]  "
+            />
 
-          <Image
-            width={200}
-            height={null}
-            src={stackRight}
-            alt=""
-            className="absolute top-40 left-[200px] z-30 h-[300px]  rounded-tl-[20px] rounded-tr-[20px]"
-          />
+            <Image
+              width={200}
+              height={null}
+              src={stackRight}
+              alt=""
+              className="absolute top-[166px] left-[250px] z-10 h-[300px]  rounded-tl-[20px] rounded-tr-[20px]"
+            />
 
-          <Image
-            width={200}
-            height={null}
-            src={stackCenter}
-            alt=""
-            className="absolute top-80 left-30 h-[300px] rounded-tl-[20px] rounded-tr-[20px]"
-          />
-        </div>
-
-        <div>
+            <Image
+              width={200}
+              height={null}
+              src={stackCenter}
+              alt=""
+              className="absolute top-[370px] left-[100px] z-20 h-[300px] rounded-tl-[20px] rounded-tr-[20px]"
+            />
+          </div>
           <Image
             width={200}
             height={null}
             src={waterMark}
             alt=""
-            className="rounded-tl-[20px] rounded-tr-[20px]"
+            className="ml-0 mt-7 z-50 "
           />
         </div>
 
-        <div className="lg:w-6/12 w-10/12 flex justify-center items-center  mx-auto">
+        <div className=" lg:w-6/12 w-10/12 flex justify-center items-center  mx-auto">
           <div className=" space-y-8 text-white">
             <p className="text-2xl lg:text-4xl font-bold">
               Discover the Creator in You
