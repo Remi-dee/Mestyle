c805ba573e0106d58b31aa5363a43a4a9a5401eb
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-02 16:22:12 +0000

    Tuesday, January 2, 2024 at 5:22 PM West Africa Standard Time

---
+++ app/profile/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PersonaliseScreen from '../components/ui/personaliseScreen/template'
+import { FormProvider } from '@/context/form/userProfile/FormContext'
+import Form from '../components/form/userProfile/template'
+
+
+function template() {
+  return (
+    <div className="flex min-h-screen items-center  justify-center">
+    <PersonaliseScreen
+        title="Get Personalized Outfit Recommendations"
+        description={`At Mestyle, we're dedicated to helping you look and feel your best. To provide you with the most relevant outfit suggestions, please share some information with us. Rest assured that your data is kept secure and used only to enhance your style journey.`}
+      >
+        <FormProvider>
+          <Form />
+        </FormProvider>
+      </PersonaliseScreen>
+    </div>
+  )
+}
+
+export default template
