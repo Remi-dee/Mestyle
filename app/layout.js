884fec68e64ba045476e122c6d2ad0c85c1cb99f
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-06 22:07:32 +0000

    Saturday, January 6, 2024 at 11:07 PM West Africa Standard Time

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Oregano } from "next/font/google";
 import "./globals.css";
 import { lexend } from "./localFonts/lexend/localFont";
+import { ThemeProvider } from "next-themes";
 
 export const metadata = {
   title: "MeStyle",
@@ -10,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${lexend.variable} bg-grayDark `}>{children}</body>
+      <body className={`${lexend.variable} bg-grayDark `}>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
