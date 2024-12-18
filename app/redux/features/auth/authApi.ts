5955f235cb0ed2a642f715e8a4875ec923c77373
Remidee <remi.aluko@expatswap.com>
2024-12-18 00:08:13 +0000

    Wednesday, 18 December 2024 at 01:08 West Africa Standard Time

---
+++ app/redux/features/auth/authApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }), // Update to match your backend's base URL
+  endpoints: (builder) => ({
+    register: builder.mutation<void, { username: string; email: string; password: string }>({
+      query: (user) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: user,
+      }),
+    }),
+    login: builder.mutation<{ accessToken: string }, { email: string; password: string }>({
+      query: (credentials) => ({
+        url: '/auth/login',
+        method: 'POST',
+        body: credentials,
+        credentials: 'include', // Include cookies for refresh token handling
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
---
+++ app/redux/features/user/user.api.ts
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../../store";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL, // base URL
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.accessToken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getAllUsers: builder.query({
+      query: () => "users",
+    }),
+    getUserPreferences: builder.query({
+      query: () => "users/preferences",
+    }),
+    updatePreferences: builder.mutation({
+      query: (preferences) => ({
+        url: "users/preferences",
+        method: "PATCH",
+        body: preferences,
+      }),
+    }),
+    getUserProfile: builder.query({
+      query: () => "users/profile",
+    }),
+  }),
+});
+
+export const {
+  useGetAllUsersQuery,
+  useGetUserProfileQuery,
+  useGetUserPreferencesQuery,
+  useUpdatePreferencesMutation,
+} = userApi;
