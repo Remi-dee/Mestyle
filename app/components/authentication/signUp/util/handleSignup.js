a9340b6b480e833a20c00e87b940f30d622f0425
Remidee <remi.aluko@expatswap.com>
2024-12-18 01:16:08 +0000

    Wednesday, 18 December 2024 at 02:16 West Africa Standard Time

--- app/components/authentication/signUp/util/handleSignup.js
+++ app/components/authentication/signUp/util/handleSignup.js
@@ -1,22 +1,36 @@
-import { appAuth, firebaseConfig } from "@/app/fireBase/firebase";
-import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRegisterMutation } from "@/redux/authApi";
+import { setCredentials } from "@/redux/authSlice";
+import { useDispatch } from "react-redux";
 
-const handleSignUp = async ({ email, password }) => {
-  try {
-    console.log(email, "see it", password);
-    const userCredential = await createUserWithEmailAndPassword(
-      appAuth,
-      email,
-      password
-    );
-    const user = userCredential.user;
-    console.log("User signed up:", user);
-    return { success: true, user };
-  } catch (error) {
-    console.error("Error signing up:", error.message);
-    return { success: false, error: error.message };
-  }
+const HandleSignUp = () => {
+  const [register] = useRegisterMutation();
+  const dispatch = useDispatch();
+
+  const signUp = async ({ username, email, password }) => {
+    try {
+      const response = await register({ username, email, password }).unwrap();
+
+      // If registration is successful, update the Redux state
+      if (response && response.accessToken) {
+        dispatch(
+          setCredentials({
+            user: { email }, // Use username if needed
+            accessToken: response.accessToken,
+          })
+        );
+        console.log("User registered and state updated:", response);
+        return { success: true };
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      return {
+        success: false,
+        error: error.data?.message || "Registration failed",
+      };
+    }
+  };
+
+  return { signUp };
 };
 
-export { handleSignUp };
+export default HandleSignUp;
